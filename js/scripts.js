e1158f2560de370c7eb9ce44f4521521365ba8a4
nuriandictionary <126185339+nuriandictionary@users.noreply.github.com>
2023-03-15 17:52:45 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -50,10 +50,11 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
+    
+    /*
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
     });
-
+    */
 });
